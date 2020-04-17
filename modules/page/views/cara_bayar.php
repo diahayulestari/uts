c9528b13a01aabcc624cf5e9588506b060352aef
diahayulestari <62719853+diahayulestari@users.noreply.github.com>
2020-04-17 09:45:36 +0700

    Add files via upload

---
+++ modules/page/views/cara_bayar.php
@@ -0,0 +1,9 @@
+<h2>Kontak</h2>
+<a class="navbar-brand" href="#"><img src="<?php echo base_url()?>assets/images/wa.png"></a>
+<p>+6283166144014</p><br>
+<a class="navbar-brand" href="#"><img src="<?php echo base_url()?>assets/images/igg.png"></a>
+<p>@a.store</p><br>
+<a class="navbar-brand" href="#"><img src="<?php echo base_url()?>assets/images/fbb.png"></a>
+<p>a store smg</p><br>
+<a class="navbar-brand" href="#"><img src="<?php echo base_url()?>assets/images/gmail.png"></a>
+<p>astore20@gmail.com</p><br>
---
+++ modules/page/views/tentang.php
@@ -0,0 +1,6 @@
+<h2>Tentang A Store</h2>
+<p>"A Store berdiri sejak tahun 2018. Nama A Store diambil dari nama pemilik toko yaitu Ayy dan Ardian.
+A Store merupakan toko online yang menyediakan fashion cassual mulai dari Hoddie, kaos, kemeja, topi hingga sepatu.
+Brand-brand cassual seperti Adidas, Fila, Polo dan lainlain ini dapat menarik minat anak muda jaman sekarang."</p>
+<p>
+</p>
